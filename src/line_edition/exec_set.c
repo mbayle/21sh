1c43faa3b6972893a75e35623692f542a4be8bf8
Frederic Ametonou <frederic.ametonou17@gmail.com>
2020-03-10 03:16:31 +0100

    maj exec_set.c

--- src/line_edition/exec_set.c
+++ src/line_edition/exec_set.c
@@ -6,7 +6,7 @@
 /*   By: frameton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 04:25:07 by frameton          #+#    #+#             */
-/*   Updated: 2020/03/10 03:02:39 by ymarcill         ###   ########.fr       */
+/*   Updated: 2020/03/10 03:14:52 by frameton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ int		exec_set(t_struct *s, char **av)
 {
 	t_lst2	*l;
 
-	(void)av;
-	if ((s->av = ft_splitws(s->cmd)) == NULL)
-		return (1);
-	if ((*s).av[1])
+	if (av[1])
 		return (0);
 	l = s->env;
 	while (l)
