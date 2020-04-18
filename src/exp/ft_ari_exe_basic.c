65ac1135040f0eb8d7b996edf3930383661b8938
Adgeff <g.argence@hotmail.fr>
2020-04-18 18:43:29 +0200

    [FIX] - Expansion arithmétique : message d'erreur quand division/modulo par 0

--- src/exp/ft_ari_exe_basic.c
+++ src/exp/ft_ari_exe_basic.c
@@ -6,7 +6,7 @@
 /*   By: geargenc <geargenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 04:49:30 by geargenc          #+#    #+#             */
-/*   Updated: 2020/04/14 06:21:29 by geargenc         ###   ########.fr       */
+/*   Updated: 2020/04/18 18:33:14 by geargenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,8 @@ int							ft_ari_exe_div(t_ari_node *node, int rec)
 	if (g_ari_exetab[node->left->token](node->left, rec) ||
 		g_ari_exetab[node->right->token](node->right, rec))
 		return (-1);
+	if (node->right->value == 0)
+		return (ft_ari_zero_divisor(node->input));
 	node->value = (node->left->value / node->right->value);
 	return (0);
 }
@@ -53,6 +55,8 @@ int							ft_ari_exe_mod(t_ari_node *node, int rec)
 	if (g_ari_exetab[node->left->token](node->left, rec) ||
 		g_ari_exetab[node->right->token](node->right, rec))
 		return (-1);
+	if (node->right->value == 0)
+		return (ft_ari_zero_divisor(node->input));
 	node->value = (node->left->value % node->right->value);
 	return (0);
 }
