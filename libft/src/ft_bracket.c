2aa566262fa0adbb6a215564699813cfff585f87
frameton <frameton@student.42.fr>
2020-04-22 20:34:32 +0200

    norme bracket

--- libft/src/ft_bracket.c
+++ libft/src/ft_bracket.c
@@ -24,8 +24,58 @@ static int match(char top, char a)
 	return (0);
 }
 
-int ft_bracket(char *str, int top, int a, char *stack)
+int		bracket_return(int top, int a, char *stack)
 {
+	if (top == -1)
+		return (a);
+	if (stack[top] == '{')
+		return (-1);
+	if (stack[top] == '(')
+		return (-2);
+	else
+		return (0);
+}
+
+int		ft_bracket_2(char *str, int a, int *top, char **stack)
+{
+	if (str[a] && (str[a] == '(' || str[a] == '{'))
+	{
+		(*top)++;
+		(*stack)[*top] = str[a];
+	}
+	if (str[a] && (str[a] == ')' || str[a] == '}'))
+	{
+		if (*top == -1 || stack[*top] == 0)
+			return (str[a] == '}' ? -1 : -2);
+		else if (*top == -1 || ((*stack)[*top] && !(match((*stack)[*top],
+		str[a]))))
+			return (str[a] == '}' ? -1 : -2);
+		else
+		{
+			(*stack)[*top] = 0;
+			(*top)--;
+		}
+	}
+	return (1);
+}
+
+int		ft_bracket_3(char *str, int *a)
+{
+	int		i;
+
+	i = (str[*a] == 34 ? ft_check_dquote(str + *a) : ft_check_squote(str + *a));
+	if (i != -1)
+		a = a + i + 1;
+	else
+		return (str[*a] == 34 ? -3 : -4);
+	return (1);
+}
+
+int		ft_bracket(char *str, int top, int a, char *stack)
+{
+	int		r;
+	int		t;
+
 	if (!str[a])
 		return (-3);
 	while (str[a])
@@ -39,41 +89,13 @@ int ft_bracket(char *str, int top, int a, char *stack)
 		}
 		if (str[a] == 34 || str[a] == 39)
 		{
-			int i = 0;
-			i = (str[a] == 34 ? ft_check_dquote(str + a) : ft_check_squote(str + a));
-			if (i != -1)
-			{
-				a = a + i + 1;
-				continue;
-			}
-			else
-				return (str[a] == 34 ? -3 : -4);
+			if ((t = ft_bracket_3(str, &a)) == -3 || t == -4)
+				return (t);
 		}
-		if (str[a] && (str[a] == '(' || str[a] == '{'))
-		{
-			top++;
-			stack[top] = str[a];
-		}
-		if (str[a] && (str[a] == ')' || str[a] == '}'))
-		{
-			if (top == -1 || stack[top] == 0)
-				return (str[a] == '}' ? -1 : -2);
-			else if (top == -1 || (stack[top] && !(match(stack[top], str[a]))))
-				return (str[a] == '}' ? -1 : -2);
-			else
-			{
-				stack[top] = 0;
-				top--;
-			}
-		}
+			r = ft_bracket_2(str, a, &top, &stack);
+			if (r == -1 || r == -2)
+				return (r);
 		a++;
 	}
-	if (top == -1)
-		return (a);
-	if (stack[top] == '{')
-		return (-1);
-	if (stack[top] == '(')
-		return (-2);
-	else
-		return (0);
+	return (bracket_return(top, a, stack));
 }
--- libft/src/ft_bracket_index.c
+++ libft/src/ft_bracket_index.c
@@ -24,8 +24,58 @@ static int match(char top, char a)
 	return (0);
 }
 
-int ft_bracket_index(char *str, int top, int a, char *stack)
+int			bracket_return(int top, char *stack)
 {
+	if (stack[top] == '{')
+		return (-1);
+	if (stack[top] == '(')
+		return (-2);
+	else
+		return (0);
+}
+
+int		ft_bracket_index2(char **stack, int *top, char *str, int a)
+{
+	if (str[a] && (str[a] == '(' || str[a] == '{'))
+	{
+		(*top)++;
+		(*stack)[*top] = str[a];
+	}
+	if (str[a] && (str[a] == ')' || str[a] == '}'))
+	{
+		if (*top == -1 || (*stack)[*top] == 0)
+			return (str[a] == '}' ? -1 : -2);
+		else if (*top == -1 || ((*stack)[*top] &&
+		!(match((*stack)[*top], str[a]))))
+			return (str[a] == '}' ? -1 : -2);
+		else
+		{
+			(*stack)[*top] = 0;
+			(*top)--;
+			if (*top == -1)
+				return (0);
+		}
+	}
+	return (1);
+}
+
+int		ft_bracket_index3(char *str, int *a)
+{
+	int		i;
+
+	i = (str[*a] == 34 ? ft_check_dquote(str + *a) :
+			ft_check_squote(str + *a));
+	if (i != -1)
+		*a = *a + i + 1;
+	else
+		return (-3);
+	return (1);
+}
+
+int		ft_bracket_index(char *str, int top, int a, char *stack)
+{
+	int		r;
+
 	if (!str[a])
 		return (-3);
 	while (str[a])
@@ -38,42 +88,14 @@ int ft_bracket_index(char *str, int top, int a, char *stack)
 				break;
 		}
 		if (str[a] == 34 || str[a] == 39)
-		{
-			int i = 0;
-			i = (str[a] == 34 ? ft_check_dquote(str + a) : ft_check_squote(str + a));
-			if (i != -1)
-			{
-				a = a + i + 1;
-				continue;
-			}
-			else
+			if (ft_bracket_index3(str, &a) == -3)
 				return (-3);
-		}
-		if (str[a] && (str[a] == '(' || str[a] == '{'))
-		{
-			top++;
-			stack[top] = str[a];
-		}
-		if (str[a] && (str[a] == ')' || str[a] == '}'))
-		{
-			if (top == -1 || stack[top] == 0)
-				return (str[a] == '}' ? -1 : -2);
-			else if (top == -1 || (stack[top] && !(match(stack[top], str[a]))))
-				return (str[a] == '}' ? -1 : -2);
-			else
-			{
-				stack[top] = 0;
-				top--;
-				if (top == -1)
-					return (a);
-			}
-		}
+		r = ft_bracket_index2(&stack, &top, str, a);
+		if (!r)
+			return (a);
+		if (r == -1 || r == -2)
+			return (r);
 		a++;
 	}
-	if (stack[top] == '{')
-		return (-1);
-	if (stack[top] == '(')
-		return (-2);
-	else
-		return (0);
+	return (bracket_return(top, stack));
 }
